50719ceaed215cad046c01c729b91533760884b5
rseghier <65013339+rseghier@users.noreply.github.com>
2025-03-10 19:46:16 +0800

    prisma ok for Explore cities

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   images: {
     domains: ['lh5.googleusercontent.com'],
   },
+  exportPathMap: async function(defaultPathMap) {
+    // Filter out all the city-specific paths from default paths
+    const filteredPaths = Object.keys(defaultPathMap)
+      .filter(path => !path.match(/^\/cities\/[^/]+$/))
+      .reduce((acc, path) => {
+        acc[path] = defaultPathMap[path];
+        return acc;
+      }, {});
+    
+    return filteredPaths;
+  },
 }
 
 module.exports = nextConfig 
